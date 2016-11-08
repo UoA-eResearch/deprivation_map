0b008619747fbb0df2bb294daaa81b3dcc8c0826
Nick Young <nick.young@auckland.ac.nz>
2016-11-08 15:38:59 +1300

    shell escape

--- seed.sh
+++ seed.sh
@@ -2,8 +2,9 @@
 mkdir seed_progress
 for c in BuPu OrRd;do
   for i in AccRank CrimeRank EduRank EmpRank HlthRank HouseRank IncomeRank NZIMDRank;do
-    echo $c_$i
-    tilestache-seed.py --config tilestache.cfg -b -30 161 -50 179.9 9 10 11 12 13 -l $c_$i -q --progress-file=seed_progress/$i.json &
+    name=${c}_${i}
+    echo $name
+    tilestache-seed.py --config tilestache.cfg -b -30 161 -50 179.9 9 10 11 12 13 -l $name -q --progress-file=seed_progress/$name.json &
   done
 done
 echo all jobs started
