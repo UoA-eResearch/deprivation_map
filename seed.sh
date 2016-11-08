349a9690ed17529a212849086a51ddc951cdb0f4
Nick Young <nick.young@auckland.ac.nz>
2016-11-08 15:34:11 +1300

    add colors to seed.sh

--- seed.sh
+++ seed.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mkdir seed_progress
-for i in AccRank CrimeRank EduRank EmpRank HlthRank HouseRank IncomeRank NZIMDRank;do
-  echo $i
-  tilestache-seed.py --config tilestache.cfg -b -30 161 -50 179.9 9 10 11 12 13 -l $i -q --progress-file=seed_progress/$i.json &
+for c in BuPu OrRd;do
+  for i in AccRank CrimeRank EduRank EmpRank HlthRank HouseRank IncomeRank NZIMDRank;do
+    echo $c_$i
+    tilestache-seed.py --config tilestache.cfg -b -30 161 -50 179.9 9 10 11 12 13 -l $c_$i -q --progress-file=seed_progress/$i.json &
+  done
 done
 echo all jobs started
